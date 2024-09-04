46bde63afba226c7be73681a882b2ed20d3efda5
UmerElsaharty <159751097+UmerElsaharty@users.noreply.github.com>
2024-09-04 21:16:54 +0300

    apps.py

--- apps.py
+++ apps.py
@@ -23,7 +23,7 @@ if not os.path.exists(MODEL_PATH):
         file.write(response.content)
 
 # Load the model
-model = load_model(MODEL_PATH)
+model = load_model(MODEL_URL)
 
 # Define class names for the model
 class_names = np.array(['CaS', 'CoS', 'Gum', 'MC', 'OC', 'OLP', 'OT'])  
